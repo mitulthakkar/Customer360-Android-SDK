489f7da4fc1d2379177cd30c28cc558f05b5c39d
mitulthakkar <mitul@customer360.co>
2015-07-24 12:38:48 +0530

    removed unwanted

--- Customer360GCMDemo/gen/com/customer360gcmdemo/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.customer360gcmdemo;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- customer360SDK/gen/cus360/inapp/helpdesk/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package cus360.inapp.helpdesk;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
